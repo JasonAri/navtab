dfa3ef8256c14a7de750bdfb866035554de63b2b
JasonAri <jason.ari@outlook.com>
2024-07-15 23:05:50 +0800

    chore: update icon, add config

---
+++ .prettierrc.mjs
@@ -0,0 +1,14 @@
+// .prettierrc.mjs
+export default {
+  printWidth: 80, // 列宽 80
+  tabWidth: 2, // 缩进 2
+  semi: false, // 分号 true
+  singleQuote: true, // 单引号 false
+  quoteProps: "as-needed", // 对象属性名添加引号 as-needed
+  jsxBracketSameLine: false, // jsx中括号是否同行 false
+  trailingComma: "es5", // 对象属性尾部逗号 es5
+  bracketSpacing: true, // 对象内侧空格 true
+  arrowParens: "avoid", // 箭头函数括号 always
+  endOfLine: "lf", // 换行符类型 lf
+  bracketLine: true, // HTML元素的标签右尖括号不单独换行 true
+};
