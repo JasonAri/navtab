01c22f3e350e7a544b29f7719b36140b4e3d0dce
JasonAri <jason.ari@outlook.com>
2024-08-06 03:38:11 +0800

    feat: add setting menu button

---
+++ src/components/SettingMenu.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="container"></div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.container {
+  width: 150px;
+  height: 200px;
+  background-color: #fff;
+  border-radius: 12px;
+  position: absolute;
+  top: 60px;
+  right: 30px;
+  transition: 0.5s;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,75 @@
 <template>
   <div class="container">
+    <SettingFilled
+      class="setting-btn"
+      @click="handleSettingBtn"
+      id="settingLogoNode"
+    />
+    <SettingMenu v-show="isShowSetting" id="settingMenuNode" />
     <SearchBar />
     <NavBar />
     <Login v-if="isLogin" />
   </div>
 </template>
 
-<script setup lang='ts'>
-import { ref } from 'vue'
+<script setup lang="ts">
+import { onBeforeUnmount, onMounted, ref, watch } from 'vue'
 import NavBar from '../components/Dock.vue'
 import SearchBar from '../components/SearchBar.vue'
 import Login from '../components/Login.vue'
+import SettingMenu from '../components/SettingMenu.vue'
+import { SettingFilled } from '@ant-design/icons-vue'
 
 const isLogin = ref(false)
+const isShowSetting = ref(false)
+const isClickOutsite = ref(false)
+
+const handleSettingBtn = () => {
+  isShowSetting.value = !isShowSetting.value
+}
+
+const handleClick = (e: MouseEvent) => {
+  // console.log(e.target)
+  if (isShowSetting.value) {
+    const settingMenuNode = document.getElementById('settingMenuNode')
+    const settingLogoNode = document.getElementById('settingLogoNode')
+    if (
+      !settingLogoNode?.contains(e.target as HTMLElement) &&
+      !settingMenuNode?.contains(e.target as HTMLElement)
+    ) {
+      handleSettingBtn()
+    }
+  }
+}
+
+watch(isClickOutsite, () => {
+  console.log(isClickOutsite.value)
+})
+
+onMounted(() => {
+  document.addEventListener('click', handleClick)
+})
+
+onBeforeUnmount(() => {
+  document.removeEventListener('click', handleClick)
+})
 </script>
 
-<style scoped lang='scss'>
+<style scoped lang="scss">
 .container {
   height: 100vh;
   overflow: hidden;
   background-image: url('/images/bg.jpg');
   background-size: cover;
+}
+.setting-btn {
+  position: absolute;
+  top: 30px;
+  right: 30px;
+  color: rgba(255, 255, 255, 0.1);
+  font-size: 24px;
+  &:hover {
+    color: rgba(255, 255, 255, 0.2);
+  }
 }
 </style>
