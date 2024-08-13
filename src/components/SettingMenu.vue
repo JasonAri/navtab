16c974891497389495ff121a36ec500f548154f8
JasonAri <jason.ari@outlook.com>
2024-08-14 04:08:16 +0800

    feat: add login btn in setting menu

--- src/components/SettingMenu.vue
+++ src/components/SettingMenu.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <div class="container"></div>
+    <div class="container">
+      <div class="btn-list">
+        <div class="login">登录/注册</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,12 +13,27 @@
 <style scoped lang="scss">
 .container {
   width: 150px;
-  height: 200px;
   background-color: #fff;
   border-radius: 12px;
   position: absolute;
   top: 60px;
   right: 30px;
-  transition: 0.5s;
+}
+.btn-list {
+  margin: 10px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.login {
+  width: 90%;
+  height: 30px;
+  line-height: 30px;
+  text-align: center;
+  padding: 5px 0;
+  border-radius: 8px;
+  &:hover {
+    background-color: #eaeaea;
+  }
 }
 </style>
