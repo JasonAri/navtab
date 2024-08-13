b47f1c7a51220d344f1e2e2b3ceb40ef35cf87b0
JasonAri <jason.ari@outlook.com>
2024-08-14 04:51:31 +0800

    refactor: transfer setting logo code to setting menu

--- src/components/SettingMenu.vue
+++ src/components/SettingMenu.vue
@@ -1,17 +1,85 @@
 <template>
-  <div>
-    <div class="container">
-      <div class="btn-list">
-        <div class="login">登录/注册</div>
+  <SettingFilled
+    class="setting-btn"
+    @click="handleSettingBtn"
+    id="settingLogoNode"
+    ref="settingLogoRef"
+  />
+  <Transition name="fade">
+    <div
+      v-show="isShowMenu"
+      class="setting-menu"
+      id="settingMenuNode"
+      ref="settingMenuRef"
+    >
+      <div class="menu-list">
+        <div class="item-login" @click="handleLoginBtn">登录/注册</div>
       </div>
     </div>
-  </div>
+  </Transition>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, watch, onMounted, onBeforeUnmount } from 'vue'
+import { SettingFilled } from '@ant-design/icons-vue'
+
+const props = defineProps({
+  showLoginPage: {
+    type: Function,
+    require: true
+  }
+})
+
+const isShowMenu = ref(false)
+const isClickOutsite = ref(false)
+const settingLogoRef: any = ref(null)
+const settingMenuRef: any = ref(null)
+
+const handleSettingBtn = () => {
+  isShowMenu.value = !isShowMenu.value
+}
+
+const handleLoginBtn = () => {
+  isShowMenu.value = false
+  props.showLoginPage?.()
+}
+
+const handleClick = (e: MouseEvent) => {
+  if (isShowMenu.value) {
+    if (
+      !settingLogoRef.value.contains(e.target as HTMLElement) &&
+      !settingMenuRef.value.contains(e.target as HTMLElement)
+    ) {
+      handleSettingBtn()
+    }
+  }
+}
+
+watch(isClickOutsite, () => {
+  console.log(isClickOutsite.value)
+})
+
+onMounted(() => {
+  document.addEventListener('click', handleClick)
+})
+
+onBeforeUnmount(() => {
+  document.removeEventListener('click', handleClick)
+})
+</script>
 
 <style scoped lang="scss">
-.container {
+.setting-btn {
+  position: absolute;
+  top: 30px;
+  right: 30px;
+  color: rgba(255, 255, 255, 0.1);
+  font-size: 24px;
+  &:hover {
+    color: rgba(255, 255, 255, 0.2);
+  }
+}
+.setting-menu {
   width: 150px;
   background-color: #fff;
   border-radius: 12px;
@@ -19,13 +87,13 @@
   top: 60px;
   right: 30px;
 }
-.btn-list {
+.menu-list {
   margin: 10px 0;
   display: flex;
   flex-direction: column;
   align-items: center;
 }
-.login {
+.item-login {
   width: 90%;
   height: 30px;
   line-height: 30px;
@@ -35,5 +103,13 @@
   &:hover {
     background-color: #eaeaea;
   }
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.25s;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="container">
-    <SettingFilled
-      class="setting-btn"
-      @click="handleSettingBtn"
-      id="settingLogoNode"
-    />
-    <SettingMenu v-show="isShowSetting" id="settingMenuNode" />
+    <SettingMenu :showLoginPage="showLoginPage" />
     <SearchBar />
     <NavBar />
     <Login v-if="isLogin" />
@@ -13,46 +8,17 @@
 </template>
 
 <script setup lang="ts">
-import { onBeforeUnmount, onMounted, ref, watch } from 'vue'
+import { ref } from 'vue'
 import NavBar from '../components/Dock.vue'
 import SearchBar from '../components/SearchBar.vue'
 import Login from '../components/Login.vue'
 import SettingMenu from '../components/SettingMenu.vue'
-import { SettingFilled } from '@ant-design/icons-vue'
 
 const isLogin = ref(false)
-const isShowSetting = ref(false)
-const isClickOutsite = ref(false)
 
-const handleSettingBtn = () => {
-  isShowSetting.value = !isShowSetting.value
+const showLoginPage = () => {
+  isLogin.value = !isLogin.value
 }
-
-const handleClick = (e: MouseEvent) => {
-  // console.log(e.target)
-  if (isShowSetting.value) {
-    const settingMenuNode = document.getElementById('settingMenuNode')
-    const settingLogoNode = document.getElementById('settingLogoNode')
-    if (
-      !settingLogoNode?.contains(e.target as HTMLElement) &&
-      !settingMenuNode?.contains(e.target as HTMLElement)
-    ) {
-      handleSettingBtn()
-    }
-  }
-}
-
-watch(isClickOutsite, () => {
-  console.log(isClickOutsite.value)
-})
-
-onMounted(() => {
-  document.addEventListener('click', handleClick)
-})
-
-onBeforeUnmount(() => {
-  document.removeEventListener('click', handleClick)
-})
 </script>
 
 <style scoped lang="scss">
@@ -61,15 +27,5 @@ onBeforeUnmount(() => {
   overflow: hidden;
   background-image: url('/images/bg.jpg');
   background-size: cover;
-}
-.setting-btn {
-  position: absolute;
-  top: 30px;
-  right: 30px;
-  color: rgba(255, 255, 255, 0.1);
-  font-size: 24px;
-  &:hover {
-    color: rgba(255, 255, 255, 0.2);
-  }
 }
 </style>
