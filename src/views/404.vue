a3d0a16ddeb1e924787e7f86e828670a6b25ef8e
JasonAri <jason.ari@outlook.com>
2024-08-17 18:38:46 +0800

    chore: edit 404 page css

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,19 +1,24 @@
 <template>
-  <Result
-    status="404"
-    title="404"
-    sub-title="Sorry, the page you visited does not exist."
-  >
-    <template #extra>
-      <RouterLink to="/home">
-        <Button type="primary"> Back Home </Button>
-      </RouterLink>
-    </template>
-  </Result>
+  <div class="container">
+    <Result status="404" title="404" sub-title="对不起，您要找的页面被丢失了">
+      <template #extra>
+        <RouterLink to="/home">
+          <Button type="primary"> Back Home </Button>
+        </RouterLink>
+      </template>
+    </Result>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { Result, Button } from 'ant-design-vue'
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.container{
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
